9bf1ff8a42c85f74a06f1b37834052df6a31e60e
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-08-12 12:28:49 +0700

    feat: Add what is seiyuu component
    Add what is seiyuu component, import noto sans jp font from google fonts, and make the page not scrollable

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/header/header.vue";
 import HeaderSeparator from "./components/separator/header-separator.vue";
+import WhatIsSeiyuu from "./components/main/what-is-seiyuu.vue";
 </script>
 
 <template>
   <Header></Header>
   <HeaderSeparator></HeaderSeparator>
+  <WhatIsSeiyuu></WhatIsSeiyuu>
 </template>
 
 <style scoped></style>
---
+++ src/components/main/what-is-seiyuu.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>What is Seiyū?</h1>
+    <p>
+      声優 (seyū or seiyuu) is the Japanese word for voice actor or voice
+      actress. Seiyū provides voice-overs for characters and narration for
+      various types of media, including anime, video games, audio dramas,
+      live-action stunt, puppet shows, and commercials.
+    </p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10%;
+  height: 100%;
+  gap: 20px;
+}
+
+h1 {
+  font-family: "Montserrat", sans-serif;
+  font-weight: bold;
+  font-size: 2rem;
+}
+
+p {
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 1.2rem;
+  width: 40%;
+  text-align: justify;
+  letter-spacing: 1px;
+  line-height: 2;
+}
+</style>
