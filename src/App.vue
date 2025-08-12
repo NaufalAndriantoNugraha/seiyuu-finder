2a5df39a9fbf912dd9c480623e895bffcb41051b
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-08-12 10:58:38 +0700

    feat: Add header separator component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/header/header.vue";
+import HeaderSeparator from "./components/separator/header-separator.vue";
 </script>
 
 <template>
   <Header></Header>
+  <HeaderSeparator></HeaderSeparator>
 </template>
 
 <style scoped></style>
---
+++ src/components/separator/header-separator.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+div {
+  background-color: #fab387;
+  height: 5px;
+}
+</style>
