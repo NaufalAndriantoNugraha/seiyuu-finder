ddac610551f8085b9109021212bd3fe9ee84d532
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-08-13 20:56:20 +0700

    feat: Add search bar on home component and vue-icons package

--- src/components/main/home.vue
+++ src/components/main/home.vue
@@ -1,7 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Fa6MagnifyingGlass } from "vue-icons-plus/fa6";
+</script>
 
 <template>
-  <h1>Hello, from home!</h1>
+  <section class="search-container">
+    <div class="search-item">
+      <Fa6MagnifyingGlass size="20" class="icon"></Fa6MagnifyingGlass>
+      <div class="separator"></div>
+      <input
+        type="text"
+        placeholder="Search seiyū's name in romaji or kanji, e.g., Natsuki Hanae or 夏樹花江"
+      />
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+section {
+  padding: 20px 20px;
+}
+
+.search-container {
+  display: flex;
+  gap: 10px;
+}
+
+.search-item {
+  display: flex;
+  border: 2px solid #333;
+  border-radius: 0.25em;
+  padding: 0.4em;
+  padding-left: 15px;
+  flex: 1;
+  gap: 10px;
+}
+
+.separator {
+  width: 2px;
+  background-color: #333;
+  border-radius: 10px;
+}
+
+.icon {
+  margin-top: 1px;
+}
+
+input {
+  border: none;
+  outline: none;
+  width: 100%;
+  font-size: 1.2rem;
+}
+</style>
