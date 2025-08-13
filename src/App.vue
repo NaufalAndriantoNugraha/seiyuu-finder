03224820643904674c31ee321acffc6f351a1fa4
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-08-13 15:49:50 +0700

    feat: Add home and about-this-project component
    Add home and about-this-project component. Also, add functionality to change the component dynamically. But, for now we still need to change the component index manually.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
 import Header from "./components/header/header.vue";
 import HeaderSeparator from "./components/separator/header-separator.vue";
+import Home from "./components/main/home.vue";
 import WhatIsSeiyuu from "./components/main/what-is-seiyuu.vue";
+import AboutThisProject from "./components/main/about-this-project.vue";
+import { ref } from "vue";
+
+const currentComponentIndex = ref<number>(0);
+const componentList = [
+  { id: 0, component: Home },
+  { id: 1, component: WhatIsSeiyuu },
+  { id: 2, component: AboutThisProject },
+];
+
+function setCurrentComponentIndex(index: number) {
+  currentComponentIndex.value = index;
+}
+
+function setCurrentComponent() {
+  return componentList.find(
+    (component) => component.id === currentComponentIndex.value,
+  )?.component;
+}
 </script>
 
 <template>
-  <Header></Header>
+  <Header :change-main-component="setCurrentComponentIndex"></Header>
   <HeaderSeparator></HeaderSeparator>
-  <WhatIsSeiyuu></WhatIsSeiyuu>
+  <component :is="setCurrentComponent()" :key="currentComponentIndex" />
 </template>
 
 <style scoped></style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  changeMainComponent: (index: number) => void;
+}>();
+</script>
 
 <template>
   <header>
     <h1>Seiyū Finder</h1>
     <nav>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">What is Seiyū?</a></li>
-        <li><a href="#">About This Project</a></li>
+        <li @click="props.changeMainComponent(0)">Home</li>
+        <li @click="props.changeMainComponent(1)">What is Seiyū?</li>
+        <li @click="props.changeMainComponent(2)">About This Project</li>
       </ul>
     </nav>
   </header>
@@ -37,15 +41,16 @@ ul {
   gap: 30px;
 }
 
-a {
+li {
   font-size: 1.1rem;
   font-weight: 500;
   color: inherit;
   text-decoration: none;
   text-underline-offset: 4px;
+  cursor: pointer;
 }
 
-a:hover {
+li:hover {
   text-decoration: underline;
 }
 </style>
