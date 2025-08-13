2c78041bee7176ef124ba03fe84ecc1e516a0a79
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-08-13 15:00:28 +0700

    feat: Add pictures of seiyuu and caption
    Add pictures of seiyuu and caption. Also, slightly modified heading and parapgraph style.

--- src/components/main/what-is-seiyuu.vue
+++ src/components/main/what-is-seiyuu.vue
@@ -2,6 +2,27 @@
 
 <template>
   <main>
+    <figure>
+      <div class="images">
+        <img
+          src="../../assets/Natsuki_Hanae.jpg"
+          alt="Picture of Natsuki Hanae"
+        />
+        <img
+          src="../../assets/Yoshitsugu_Matsuoka_松岡_禎丞_2022.jpg"
+          alt="Picture of Matsuoka Yoshitsugu"
+        />
+        <img src="../../assets/Kito_Akari.jpg" alt="Picture of Kito Akari" />
+        <img
+          src="../../assets/Hiro_Shimono.jpg"
+          alt="Picture of Shimono Hiro"
+        />
+      </div>
+      <figcaption>
+        Pictures of Natsuki Hanae, Matsuoka Yoshitsugu, Kito Akari, and Simono
+        Hiro (from left to right) who are famous seiyū.
+      </figcaption>
+    </figure>
     <h1>What is Seiyū?</h1>
     <p>
       声優 (seyū or seiyuu) is the Japanese word for voice actor or voice
@@ -17,20 +38,47 @@ main {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 10%;
+  padding-top: 5%;
   height: 100%;
   gap: 20px;
+}
+
+figure {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 20px;
+}
+
+.images {
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+}
+
+img {
+  width: 130px;
+  height: auto;
+}
+
+figcaption {
+  width: 55%;
+  text-align: center;
+  font-size: 0.8rem;
+  font-family: "Noto Sans JP", sans-serif;
+  letter-spacing: 1px;
 }
 
 h1 {
   font-family: "Montserrat", sans-serif;
   font-weight: bold;
-  font-size: 2rem;
+  font-size: 1.6rem;
 }
 
 p {
   font-family: "Noto Sans JP", sans-serif;
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   width: 40%;
   text-align: justify;
   letter-spacing: 1px;
